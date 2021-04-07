4f972e2162a3db257c6e2b77f0157eb5ae975155
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2021-04-07 16:35:00 -0300

    Adds missing e2e attribute

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,20 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const linkStyle = {
-  marginRight: 15
-}
+  marginRight: 15,
+};
 
 export default function Header() {
   return (
     <div>
       <Link href="/">
-        <a style={linkStyle}>Home</a>
+        <a style={linkStyle} data-e2e="header-home">
+          Home
+        </a>
       </Link>
       <Link href="/about">
         <a style={linkStyle}>About</a>
       </Link>
     </div>
-  )
+  );
 }
